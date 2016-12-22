767e632bb731e950fa261a551371995e33f53f3f
zelkatani <zelkatani@gmail.com>
2016-12-21 23:23:28 -0500

    field spreading

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var canvas = document.createElement("canvas"),
     ctx = canvas.getContext("2d");
 
 var img = new Image();
 img.onload = function () {
     setup(960, 800);
-}
+};
 img.src = "assets/sheet.png";
 
 var faces = { 
@@ -16,27 +18,30 @@ var faces = {
     loss: [80, 0],
     question: [96, 0],
     questionPressed: [112, 0],
-    one: [0, 16],
-    two: [16, 16],
-    three: [32, 16],
-    four: [48, 16],
-    five: [64, 16],
-    six: [80, 16],
-    seven: [96, 16],
-    eight: [112, 16]
+    "1": [0, 16],
+    "2": [16, 16],
+    "3": [32, 16],
+    "4": [48, 16],
+    "5": [64, 16],
+    "6": [80, 16],
+    "7": [96, 16],
+    "8": [112, 16]
 };
 
 var B_HEIGHT = 16,
     B_WIDTH = 16,
-    MINE_CHANCE = .25;
+    MINE_CHANCE = 1 / 8;
 
 var boxes = [];
 
 function Box() {
-    var xpos, ypos;
+    var xpos,
+        ypos;
 
-    var isMine = false,
-        flagged = false;
+    var isMine,
+        flagged,
+        clicked,
+        exposed = false;
 
     this.state = faces.box;
 }
@@ -54,13 +59,13 @@ var header = function () {
             face.id = "pressed";
         }, false);
 
-        face.addEventListener("mouseleave", function() {
+        face.addEventListener("mouseleave", function () {
             if (face.id === "pressed") {
                 face.id = "normal";
             }
         }, false);
 
-        face.addEventListener("click", function() {
+        face.addEventListener("click", function () {
             face.id = "normal";
         }, false);
 
@@ -81,13 +86,10 @@ function setup(width, height) {
 
     document.body.appendChild(header());
     document.body.appendChild(content);
-    
-    ctx.fillStyle = "gray";
-    ctx.fillRect(0, 0, 960, 800);
 
     canvas.addEventListener("click", function (evt) {
         var mousePos = function (evt) {
-            rect = canvas.getBoundingClientRect();
+            var rect = canvas.getBoundingClientRect();
             return {
                 x: evt.clientX - rect.left,
                 y: evt.clientY - rect.top
@@ -101,21 +103,23 @@ function setup(width, height) {
             y: Math.floor(pos.y / 16) * 16
         };
 
-        console.log(fieldPos.x + " " + fieldPos.y);
-
-        b = getBox(fieldPos.x, fieldPos.y);
-        b.state = faces.press;
+        var b = getBox(fieldPos.x, fieldPos.y);
         if (b.isMine) {
-            b.state = faces.mine;
+            b.state = faces.loss;
+            b.clicked = true;
+        }
+        if (b.state == faces.box) {
+            b.state = faces.press;
+            b.clicked = true;
         }
 
+        determineStates(b);
     }, false);
     drawGrid();
-    requestAnimationFrame(update);
 }
 
 function drawGrid() {
-    for (var i = 0; i < canvas.width; i += B_WIDTH) {   
+    for (var i = 0; i < canvas.width; i += B_WIDTH) {
         for (var j = 0; j < canvas.height; j += B_HEIGHT) {
             drawBox(i, j);
         }
@@ -123,11 +127,11 @@ function drawGrid() {
 }
 
 function drawBox(x, y) {
-    b = new Box();
+    var b = new Box();
     b.xpos = x;
     b.ypos = y;
 
-    if (Math.random() > MINE_CHANCE) b.isMine = true;
+    if (Math.random() < MINE_CHANCE) b.isMine = true;
 
     ctx.drawImage(img, b.state[0], b.state[1], B_WIDTH, B_HEIGHT, x, y, B_WIDTH, B_HEIGHT);
 
@@ -135,12 +139,72 @@ function drawBox(x, y) {
 }
 
 function getBox(x, y) {
-     for (var i = 0; i < boxes.length; i++) {
+    for (var i = 0; i < boxes.length; i++) {
         if (boxes[i].xpos == x && boxes[i].ypos == y) return boxes[i];
-    }   
+    }
     return null;
 }
 
-function update() {
-    requestAnimationFrame(update);
+function getNeighbors(box) {
+    var x = box.xpos,
+        y = box.ypos;
+
+    var nearby = [];
+    for (var i = -1; i < 2; i++) {
+        for (var j = -1; j < 2; j++) {
+            nearby.push([x + i * 16, y + j * 16]);
+        }
+    }
+    nearby.splice(4, 1); // remove coordinates that contain self
+
+    var nearBounds = nearby.map(function (b) {
+        return getBox(b[0], b[1]);
+    }).filter(function (b) {
+        return b
+    });
+
+    return nearBounds;
+}
+
+function determineStates(box) {
+    if (box.clicked && box.isMine) {
+        box.state = faces.loss;
+        redrawBox(box);
+        exposeField();
+    } else {
+        var neighbors = getNeighbors(box).filter(function (b) {
+            return !b.exposed;
+        });
+        var nearbyMines = 0;
+
+        for (var i = 0; i < neighbors.length; i++) {
+            if (neighbors[i].isMine) nearbyMines++;
+        }
+
+        if (nearbyMines > 0) {
+            box.state = faces[nearbyMines];
+        } else {
+            box.state = faces.press;
+            for (var i = 0; i < neighbors.length; i++) {
+                neighbors[i].clicked = true;
+                box.exposed = true;
+                determineStates(neighbors[i]);
+            }
+        }
+    }
+    redrawBox(box);
+}
+
+function redrawBox(box) {
+    ctx.clearRect(box.xpos, box.ypos, B_WIDTH, B_HEIGHT);
+    ctx.drawImage(img, box.state[0], box.state[1], B_WIDTH, B_HEIGHT, box.xpos, box.ypos, B_WIDTH, B_HEIGHT);
+}
+
+function exposeField() {
+    for (var i = 0; i < boxes.length; i++) {
+        if (!boxes[i].clicked && boxes[i].isMine) {
+            boxes[i].state = faces.mine;
+            redrawBox(boxes[i]);
+        }
+    }
 }
